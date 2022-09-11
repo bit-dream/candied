971c9be5658243696b5ff6bdc7a8d8e41e421562
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-09-11 18:27:43 -0400

    Added great test converage for writer class

--- src/__tests__/writer.test.ts
+++ src/__tests__/writer.test.ts
@@ -1,19 +1,108 @@
 import Dbc from '../dbc/dbc';
 import { MessageDoesNotExist } from '../dbc/errors';
 import Writer from '../dbc/writer';
+import * as fs from 'fs';
 
-test('DBC_template.dbc: Correct Messages', () => {
+test('DBC_template.dbc: Correct Messages Written to File', (done) => {
+  const testFile = 'src/__tests__/testFiles/test.dbc'
   const dbc = new Dbc();
   dbc.load('src/__tests__/testFiles/DBC_template.dbc').then((data) => {
     /* To test that we wrote the messages correctly, we will
     first parse the original file, write the parsed contents of the file
     to a new file and reparse the test file. Messages in the original file
     should match the parsed test file */
-    const writer = new Writer('src/__tests__/testFiles/test.dbc');
+    const writer = new Writer(testFile);
     writer.constructFile(data);
+
     const writerDbc = new Dbc();
-    dbc.load('test.dbc').then(writerData => {
-      expect(data.messages).toEqual(writerData.messages);
+    writerDbc.load(testFile).then(writerData => {
+      try {
+        expect(writerData.messages).toEqual(data.messages);
+        done();
+      } finally {
+        fs.unlinkSync(testFile);
+      }
+    })
+  });
+});
+
+test('DBC_template.dbc: Correct Signals Written to File', (done) => {
+  const testFile = 'src/__tests__/testFiles/test.dbc'
+  const dbc = new Dbc();
+  dbc.load('src/__tests__/testFiles/DBC_template.dbc').then((data) => {
+    /* To test that we wrote the messages correctly, we will
+    first parse the original file, write the parsed contents of the file
+    to a new file and reparse the test file. Messages in the original file
+    should match the parsed test file */
+    const writer = new Writer(testFile);
+    writer.constructFile(data);
+
+    const writerDbc = new Dbc();
+    writerDbc.load(testFile).then(writerData => {
+      try {
+        for (const [name, message] of writerData.messages) {
+          expect(writerData.messages.get(name)?.signals).toEqual(data.messages.get(name)?.signals);
+        }
+        done();
+      } finally {
+        fs.unlinkSync(testFile);
+      }
+    })
+  });
+});
+
+test('DBC_template.dbc: Correct Table Values Written to File', (done) => {
+  const testFile = 'src/__tests__/testFiles/test.dbc'
+  const dbc = new Dbc();
+  dbc.load('src/__tests__/testFiles/DBC_template.dbc').then((data) => {
+    /* To test that we wrote the messages correctly, we will
+    first parse the original file, write the parsed contents of the file
+    to a new file and reparse the test file. Messages in the original file
+    should match the parsed test file */
+    const writer = new Writer(testFile);
+    writer.constructFile(data);
+
+    const writerDbc = new Dbc();
+    writerDbc.load(testFile).then(writerData => {
+      try {
+        expect(writerData.valueTables).toEqual(data.valueTables);
+        done();
+      } finally {
+        fs.unlinkSync(testFile);
+      }
+    })
+  });
+});
+
+test('DBC_template.dbc: Correct Signals Tables Written to File', (done) => {
+  const testFile = 'src/__tests__/testFiles/test.dbc'
+  const dbc = new Dbc();
+  dbc.load('src/__tests__/testFiles/DBC_template.dbc').then((data) => {
+    /* To test that we wrote the messages correctly, we will
+    first parse the original file, write the parsed contents of the file
+    to a new file and reparse the test file. Messages in the original file
+    should match the parsed test file */
+    const writer = new Writer(testFile);
+    writer.constructFile(data);
+
+    const writerDbc = new Dbc();
+    writerDbc.load(testFile).then(writerData => {
+      try {
+        for (const [name, message] of writerData.messages) {
+          const writerSignals = writerData.messages.get(name)?.signals;
+          const dataSignals = data.messages.get(name)?.signals;
+          if (dataSignals) {
+            for (const [name, signal] of dataSignals) {
+              expect(signal.valueTable).toEqual(writerSignals?.get(name)?.valueTable);
+            }
+          } else {
+            fail('No signals in test dbc file.');
+          }
+        }
+        done();
+      } finally {
+        fs.unlinkSync(testFile);
+      }
     })
   });
 });
