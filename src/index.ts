4680c863a0fd9d7eb0c4f1abf37adfbab265cd6c
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-09-12 19:22:43 -0400

    Updated package version and main file. index.ts now exports Dbc class so that it can be imported via a package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-/*
-  This file is intentially left empty
-*/
+/**
+ * Main file
+ * Simply exports the main uility class so that it can be referenced when imported via an NPM package
+ */
+import Dbc from './dbc/dbc';
+module.exports = Dbc;
