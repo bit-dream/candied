3be8c02d3c7d6a59c62e045f9d6d2f1ef31e9f22
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-09-13 20:37:55 -0400

    Fixed bug where you could not import main Dbc class using import statements. Also fixed bug where autocomplete would not work in vscode due to using module.exports

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@
  * Simply exports the main uility class so that it can be referenced when imported via an NPM package
  */
 import Dbc from './dbc/dbc';
+
 export default Dbc;
+
+if (typeof module === "object" && typeof module.exports === "object") {
+  module.exports = Object.assign(module.exports.default, module.exports);
+}
