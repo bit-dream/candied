02e3fcfd2de51c85a6c9e4638ce9bf1e67344910
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-09-15 21:21:10 -0400

    fixed bug where parseFloat should of been used for extracting factor, offsets, min and maxes

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,16 @@ if (typeof module === 'object' && typeof module.exports === 'object') {
   module.exports = Object.assign(module.exports.default, module.exports);
 }
 
+export {Can};
+
 let dbc = new Dbc();
 
 dbc.load('src/__tests__/testFiles/tesla_can.dbc')
 .then(data => {
-  let msg = dbc.getMessageById(792);
-  let sig = dbc.getSignalByName('BOOT_STATE','GTW_carState');
-  console.log(sig);
+  let msg = dbc.getMessageById(264);
+  console.log(msg);
   const canBus = new Can(data);
-  let frame = canBus.createFrame(309,[13,100,1,10,240]);
+  let frame = canBus.createFrame(264,[13,100,140,10,240, 100, 80, 244]);
   let bndMsg = canBus.decode(frame);
   console.log(bndMsg);
 
