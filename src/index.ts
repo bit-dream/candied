1dec8edd09dcf457e3d1e8ffe4441c639da19851
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-12-03 12:28:15 -0500

    Added better support for attribute parsing

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@ import Dbc from './dbc/dbc';
 import Can from './can/can';
 import { parse } from './parser/parser'
 
+import * as fs from 'fs';
+import * as path from 'path';
+import * as readline from 'readline';
+
 export default Dbc;
 
 if (typeof module === 'object' && typeof module.exports === 'object') {
@@ -13,19 +17,9 @@ if (typeof module === 'object' && typeof module.exports === 'object') {
 }
 export { Can };
 
-const ret = parse("BO_ 4321 CANMultiplexed: 2 Node0");
-const ret2 = parse("BU_: hello CANMultiplexed ;");
-const ret3 = parse("BU_: ;");
-const ret4 = parse("BU_:");
-const ret5 = parse('SG_ Multiplexer M : 0|8@1+ (1,0) [0|0] "%" Node1')
-const ret6 = parse('SG_ Multiplexer m2 : 0|8@1+ (1,0) [0|0] "" Node1')
-const ret7 = parse('SG_ Multiplexer : 0|8@1+ (1,0) [0|0] "" Node1 Node2')
-const ret8 = parse('VAL_TABLE_ DI_mode 2 "DI_MODE_DYNO" 1 "DI_MODE_DRIVE" 0 "DI_MODE_UNDEF" ;')
-const ret9 = parse('VAL_TABLE_ DI_limpReason 24 "DI_LIMP_NUMREASONS" 23 "DI_LIMP_CAPACITOR_OVERTEMP" 22 "DI_LIMP_GTW_MIA" 21 "DI_LIMP_TRQCMD_VALIDITY_UNKNOWN" 20 "DI_LIMP_DI_MIA" 19 "DI_LIMP_CONFIG_MISMATCH" 18 "DI_LIMP_HEATSINK_TEMP" 17 "DI_LIMP_PMREQUEST" 16 "DI_LIMP_PMHEARTBEAT" 15 "DI_LIMP_TRQ_CROSS_CHECK" 14 "DI_LIMP_EXTERNAL_COMMAND" 13 "DI_LIMP_WRONG_CS_CALIBRATION" 12 "DI_LIMP_STATOR_TEMP" 11 "DI_LIMP_DELTAT_TOO_NEGATIVE" 10 "DI_LIMP_DELTAT_TOO_POSITIVE" 9 "DI_LIMP_AMBIENT_TEMP" 8 "DI_LIMP_OUTLET_TEMP" 7 "DI_LIMP_LOW_FLOW" 6 "DI_LIMP_BMS_MIA" 5 "DI_LIMP_12V_SUPPLY_UNDERVOLTAGE" 4 "DI_LIMP_NO_FLUID" 3 "DI_LIMP_NO_FUNC_HEATSINK_SENSOR" 2 "DI_LIMP_NO_FUNC_STATORT_SENSOR" 1 "DI_LIMP_BUSV_SENSOR_IRRATIONAL" 0 "DI_LIMP_PHASE_IMBALANCE" ;')
-const ret10 = parse('VAL_TABLE_ StW_AnglHP_Spd 16383 "SNA" ;')
-const ret11 = parse('VAL_ 3 StW_Angl 16383 "SNA" ;');
-const ret12 = parse('VAL_ 3 StW_AnglSens_Id 2 "MUST" 0 "PSBL" 1 "SELF" ;');
-const ret13 = parse('CM_ SG_ 1234 Signal0 "First signal in this message";');
-const ret14 = parse('CM_ BU_ Node0 "The 0th Node";');
-const ret15 = parse('CM_ BO_ 4321 "Multiplexed CAN-Message";');
-console.log(ret13,ret14,ret15);
+const fileContents = fs.readFileSync('/Users/headquarters/Documents/Code/can-dbc/src/__tests__/testFiles/DBC_template.dbc', { encoding: 'ascii' });
+const lines = fileContents.split('\n');
+lines.forEach((line) => {
+  let ret = parse(line);
+  console.log(ret)
+});
--- src/parser/parserHelpers.ts
+++ src/parser/parserHelpers.ts
@@ -16,3 +16,104 @@ export function cleanComment(comment: string): string {
   let final = noSemiColon.replace(/"/gi,'');
   return final;
 }
+
+export function extractAttrType(str: string): string {
+  const match = str.match(/BO_|BU_|SG_/)
+  if (match) {
+    switch (match?.toString()) {
+      case 'BO_':
+        return 'Message';
+      case 'BU_':
+        return 'Node';
+      case 'SG_':
+        return 'Signal';
+      default:
+        return 'Global';
+    }
+  } else {
+    return 'Global'
+  }
+}
+
+export function extractAttrNode(type: string, str: string): string {
+  let matches: RegExpMatchArray | null;
+  switch (type) {
+    case 'Message':
+      return ''
+    case 'Signal':
+      return ''
+    case 'Node':
+      matches = str.match(/BU_\s(?<node>[a-zA-Z0-9_]+)\s(?<value>.*)\s*;/);
+      if (matches) {
+        if (matches.groups) {
+          return matches.groups.node;
+        }
+      }
+    case 'Global':
+      return ''
+    default:
+      return ''
+  }
+}
+
+export function extractAttrVal(type: string, str: string): string {
+  let matches: RegExpMatchArray | null;
+  switch (type) {
+    case 'Message':
+      matches = str.match(/BO_\s(?<id>[0-9]+)\s(?<value>.*);/);
+      if (matches) {
+        if (matches.groups) {
+          return cleanComment(matches.groups.value);
+        }
+      }
+    case 'Signal':
+      matches = str.match(/SG_\s(?<id>[0-9]+)\s(?<signalName>[a-zA-Z0-9_]+)\s(?<value>.*);/);
+      if (matches) {
+        if (matches.groups) {
+          return cleanComment(matches.groups.value);
+        }
+      }
+    case 'Node':
+      matches = str.match(/BU_\s(?<node>[a-zA-Z0-9_]+)\s(?<value>.*)\s*;/);
+      if (matches) {
+        if (matches.groups) {
+          return cleanComment(matches.groups.value);
+        }
+      }
+    case 'Global':
+      matches = str.match(/(?<value>.*)\s*;/);
+      if (matches) {
+        if (matches.groups) {
+          return cleanComment(matches.groups.value);
+        }
+      }
+    default:
+      return ''
+  }
+}
+
+export function extractAttrId(type: string, str: string): string {
+  let matches: RegExpMatchArray | null;
+  switch (type) {
+    case 'Message':
+      matches = str.match(/BO_\s(?<id>[0-9]+)\s(?<value>.*);/);
+      if (matches) {
+        if (matches.groups) {
+          return matches.groups.id;
+        }
+      }
+    case 'Signal':
+      matches = str.match(/SG_\s(?<id>[0-9]+)\s(?<signalName>[a-zA-Z0-9_]+)\s(?<value>.*);/);
+      if (matches) {
+        if (matches.groups) {
+          return matches.groups.id;
+        }
+      }
+    case 'Node':
+      return ''
+    case 'Global':
+      return ''
+    default:
+      return ''
+  }
+}
