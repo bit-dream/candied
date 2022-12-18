36ed0f47a8974d52d87d4cdc78934de20332ac3f
bit-dream <61420422+bit-dream@users.noreply.github.com>
2022-12-18 10:48:09 -0500

    Cleaned up decode tests with more intelligent grouping of tests.

--- src/__tests__/decode.test.ts
+++ src/__tests__/decode.test.ts
@@ -22,94 +22,65 @@ const dummyData: DbcData = {
   networkBridges: new Map(),
 };
 
-test('Get Value: Unsigned/Intel', () =>{
+test('Get Value: Unsigned Motorola', () =>{
   const can = new Can();
-
-  // Settings for decode
-  const payload = [20,255,255,2,4,13,16,255];
-  const startbit = 36;
-  const bitLength = 23;
-  const byteOrder = 'Intel';
-  const signed = false;
-
-  const value = can.getValue(payload,startbit,bitLength,byteOrder,signed);
-  expect(value).toEqual(7405776)
-})
-
-test('Get Value: [20,255,255,2,4,13,16,255]', () =>{
-  const can = new Can();
-
-  // Settings for decode
-  const payload = [20,255,255,2,4,13,16,255];
-
-  // Signal1
-  expect(can.getValue(payload,32,20,'Motorola',false))
+  expect(can.getValue([20,255,255,2,4,13,16,255],32,20,'Motorola',false))
       .toBe(983556);
-  // Signal2
-  expect(can.getValue(payload,14,10,'Motorola',false))
+  expect(can.getValue([20,255,255,2,4,13,16,255],14,10,'Motorola',false))
       .toBe(83);
-  // Signal3
-  expect(can.getValue(payload,50,4,'Motorola',false))
+  expect(can.getValue([20,255,255,2,4,13,16,255],50,4,'Motorola',false))
       .toBe(4);
-  // Signal4
-  expect(can.getValue(payload,40,9,'Intel',false))
-      .toBe(13);
-  // Signal5
-  expect(can.getValue(payload,54,10,'Intel',true))
-      .toBe(-4);
-  // Signal6
-  expect(can.getValue(payload,22,6,'Motorola',true))
-      .toBe(-1);
-  expect(can.getValue(payload,20,27,'Intel',true))
-      .toBe(13647919);
-  expect(can.getValue(payload,13,32,'Intel',true))
-      .toBe(1746933759);
-  expect(can.getValue(payload,56,3,'Intel',true))
-      .toBe(-1);
+  expect(can.getValue([54,20,45,2], 10, 10, 'Motorola', false))
+      .toBe(389);
 })
 
-test('Get Value: [20,255,65,54,3,20,55,45]', () => {
+test('Get Value: Signed Motorola', () =>{
   const can = new Can();
-
-  // Settings for decode
-  const payload = [20, 255, 65, 54, 3, 20, 55, 45];
-
-  expect(can.getValue(payload, 22, 10, 'Motorola', true))
+  expect(can.getValue([20,255,255,2,4,13,16,255],22,6,'Motorola',true))
+      .toBe(-1);
+  expect(can.getValue([20, 255, 65, 54, 3, 20, 55, 45], 22, 10, 'Motorola', true))
       .toBe(-3);
+  expect(can.getValue([54,20,45,2], 10, 10, 'Motorola', true))
+      .toBe(389);
 })
 
-test('Get Value: [54,20,45,2]', () => {
+test('Get Value: Unsigned Intel', () =>{
   const can = new Can();
-
-  // Settings for decode
-  const payload = [54,20,45,2];
-
-  expect(can.getValue(payload, 10, 10, 'Motorola', false))
-      .toBe(389);
-  expect(can.getValue(payload, 10, 10, 'Intel', false))
+  expect(can.getValue([20,255,255,2,4,13,16,255],40,9,'Intel',false))
+      .toBe(13);
+  expect(can.getValue([54,20,45,2], 10, 10, 'Intel', false))
       .toBe(837);
-  expect(can.getValue(payload, 10, 10, 'Motorola', true))
-      .toBe(389);
-  expect(can.getValue(payload, 10, 10, 'Intel', true))
+})
+
+test('Get Value: Signed Intel', () =>{
+  const can = new Can();
+  expect(can.getValue([20,255,255,2,4,13,16,255],20,27,'Intel',true))
+      .toBe(13647919);
+  expect(can.getValue([20,255,255,2,4,13,16,255],13,32,'Intel',true))
+      .toBe(1746933759);
+  expect(can.getValue([20,255,255,2,4,13,16,255],56,3,'Intel',true))
+      .toBe(-1);
+  expect(can.getValue([54,20,45,2], 10, 10, 'Intel', true))
       .toBe(-187);
 })
 
-/*
+
 test('Can Frame Creation', () => {
-  const can = new Can(dummyData);
-  expect(can.createFrame(100, [100, 100, 100])).toStrictEqual({
+  const can = new Can();
+  can.dbc = dummyData;
+  expect(can.createFrame(100, [100, 100, 100], false)).toStrictEqual({
     id: 100,
     dlc: 3,
     isExtended: false,
     payload: [100, 100, 100],
   });
-  expect(can.createFrame(54092340, [100, 100, 100, 10])).toStrictEqual({
+  expect(can.createFrame(54092340, [100, 100, 100, 10], true)).toStrictEqual({
     id: 54092340,
     dlc: 4,
     isExtended: true,
     payload: [100, 100, 100, 10],
   });
-  expect(can.createFrame(10, [300, 300])).toStrictEqual({
+  expect(can.createFrame(10, [300, 300], false)).toStrictEqual({
     id: 10,
     dlc: 2,
     isExtended: false,
@@ -118,12 +89,12 @@ test('Can Frame Creation', () => {
 });
 
 test('Get Value From Payload', () => {
-  const can = new Can(dummyData);
-  expect(can.createFrame(100, [100, 100, 100])).toStrictEqual({
+  const can = new Can();
+  can.dbc = dummyData;
+  expect(can.createFrame(100, [100, 100, 100], false)).toStrictEqual({
     id: 100,
     dlc: 3,
     isExtended: false,
     payload: [100, 100, 100],
   });
 });
-*/
